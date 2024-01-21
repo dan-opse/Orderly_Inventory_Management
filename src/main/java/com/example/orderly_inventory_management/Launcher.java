e8f50e54a07637300220dd68fcc33d447b6473f6
dan-opse <tanneddan@gmail.com>
2024-01-20 22:05:04 -0500

    Packaged as .jar, working on .exe

---
+++ src/main/java/com/example/orderly_inventory_management/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.orderly_inventory_management;
+
+public class Launcher {
+
+    // Launches the other class because JavaFX cannot be built as an artifact otherwise?!!
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
